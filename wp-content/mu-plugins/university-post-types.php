d16ddb7d62c31dcea890661e10b35e1101ac2a56
Viktoria Juhasz <viktoriajuhasz8@gmail.com>
2020-05-05 13:19:32 +0200

    custom post types (event type)

---
+++ wp-content/mu-plugins/university-post-types.php
@@ -0,0 +1,18 @@
+<?php
+
+function university_post_types()
+{
+    register_post_type('event', [
+        'public' => true, 
+        'labels' => [
+            'name' => 'Events', 
+            'add_new_item' => 'Add New Event',
+            'edit_item' => 'Edit Event', 
+            'all_items' => 'All events', 
+            'singular_name' => 'Event'
+        ], 
+        'menu_icon' => 'dashicons-calendar'
+    ]);
+}
+
+add_action('init', 'university_post_types');
