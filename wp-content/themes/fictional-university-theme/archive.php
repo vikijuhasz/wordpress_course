7f2433a2b610eb2ecec3ee04b3507f141771207b
Viktoria Juhasz <viktoriajuhasz8@gmail.com>
2020-05-11 08:43:10 +0200

    using the pageBanner() function

--- wp-content/themes/fictional-university-theme/archive.php
+++ wp-content/themes/fictional-university-theme/archive.php
@@ -1,24 +1,32 @@
-<?php get_header(); ?>
+<?php 
 
-<div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg')?>"></div>
-      <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title">
-            <?php 
-                the_archive_title();
-                // if (is_category()) {
-                //     single_cat_title();
-                // }
-                // elseif (is_author()) {
-                //     echo 'Posts by '; the_author();
-                // }            
-            ?>
-        </h1>
-        <div class="page-banner__intro">
-          <p><?php the_archive_description(); ?></p>
-        </div>
-      </div>  
-  </div>
+  get_header(); 
+  pageBanner([
+    'title' => get_the_archive_title(),
+    'subtitle' => get_the_archive_description()
+  ]);
+  
+?>
+
+<!-- <div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg')?>"></div>
+    <div class="page-banner__content container container--narrow">
+      <h1 class="page-banner__title">
+          <?php 
+              the_archive_title();
+              // if (is_category()) {
+              //     single_cat_title();
+              // }
+              // elseif (is_author()) {
+              //     echo 'Posts by '; the_author();
+              // }            
+          ?>
+      </h1>
+      <div class="page-banner__intro">
+        <p><?php the_archive_description(); ?></p>
+      </div>
+    </div>  
+  </div> -->
 
   <div class="container container--narrow page-section">
   <?php 
