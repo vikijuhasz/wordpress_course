226060970153e8b1511d0ef87c7ca9eba3845652
Viktoria Juhasz <viktoriajuhasz8@gmail.com>
2020-05-11 11:43:45 +0200

    get_template_part()

--- wp-content/themes/fictional-university-theme/single-program.php
+++ wp-content/themes/fictional-university-theme/single-program.php
@@ -1,30 +1,30 @@
-<?php 
-    
+<?php
+
     get_header();
 
     while(have_posts()) {
-        the_post(); 
+        the_post();
         pageBanner();
-        
+
         ?>
-        
+
         <div class="container container--narrow page-section">
             <div class="metabox metabox--position-up metabox--with-home-link">
             <p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('program'); ?>"><i class="fa fa-home" aria-hidden="true"></i> All Programs</a> <span class="metabox__main"><?php the_title(); ?></span></p>
             </div>
             <div class="generic-content"><?php the_content(); ?></div>
 
-        <?php 
+        <?php
 
           $relatedProfessors = new WP_Query([
             'posts_per_page' => -1,
             'post_type' => 'professor',
             'orderby' => 'title',
-            'order' => 'ASC', 
+            'order' => 'ASC',
             'meta_query' => [
               [
                 'key' => 'related_programs',
-                'compare' => 'LIKE', 
+                'compare' => 'LIKE',
                 'value' => '"' . get_the_ID() . '"',
               ]
             ]
@@ -44,35 +44,35 @@
                     <span class="professor-card__name"><?php the_title(); ?></span>
                   </a>
                 </li>
-                <?php 
-            } 
+                <?php
+            }
             echo '</ul>';
-          } 
+          }
 
           wp_reset_postdata();
-        
+
           $today = date('Ymd');
           $homepageEvents = new WP_Query([
               'posts_per_page' => 2,
               'post_type' => 'event',
               'meta_key' => 'event_date',
               'orderby' => 'meta_value_num',
-              'order' => 'ASC', 
+              'order' => 'ASC',
               'meta_query' => [
                 [
                   'key' => 'event_date',
-                  'compare' => '>=', 
+                  'compare' => '>=',
                   'value' => $today,
                   'type' => 'numeric'
-                ], 
+                ],
                 [
                   'key' => 'related_programs',
-                  'compare' => 'LIKE', 
+                  'compare' => 'LIKE',
                   'value' => '"' . get_the_ID() . '"',
                 ]
               ]
           ]);
-
+          
          if ($homepageEvents->have_posts()) {
           echo '<hr class="section-break">';
           echo '<h2 class="headline headline--medium">Upcoming '. get_the_title() . ' Events </h2>';
@@ -84,39 +84,39 @@
           <div class="event-summary">
             <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
               <span class="event-summary__month">
-                <?php 
+                <?php
                   $eventDate = new DateTime(get_field('event_date'));
-                  echo $eventDate->format('M'); 
+                  echo $eventDate->format('M');
                 ?>
               </span>
               <span class="event-summary__day">
-                <?php 
+                <?php
                   $eventDate = new DateTime(get_field('event_date'));
-                  echo $eventDate->format('d'); 
+                  echo $eventDate->format('d');
                 ?>
-              </span>  
+              </span>
             </a>
             <div class="event-summary__content">
               <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
               <p>
-              <?php 
+              <?php
                 if (has_excerpt()) {
                   echo get_the_excerpt();
                 } else {
                   echo wp_trim_words(get_the_content(), 18);
-                } 
+                }
               ?>
               <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
             </div>
-          </div>  
+          </div>
 
-        <?php 
-          } 
-        } 
+        <?php
+          }
+        }
         ?>
         </div>
-    <?php 
-    } 
+    <?php
+    }
 
     get_footer();
 
