339b7f1630246954d425220d1339aaa870840596
Viktoria Juhasz <viktoriajuhasz8@gmail.com>
2020-05-13 12:39:51 +0200

    keyboard events in javaScript

--- wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -9,6 +9,7 @@ class Search
         this.closeButton = $(".search-overlay__close");
         this.searchOverlay = $(".search-overlay");
         this.events();
+        this.isOverlayOpen = false;
     }
 
     // 2. events
@@ -16,17 +17,33 @@ class Search
     {
         this.openButton.on("click", this.openOverlay.bind(this));
         this.closeButton.on("click", this.closeOverlay.bind(this));
+        $(document).on("keydown", this.keyPressDispatcher.bind(this));
     }
 
     // 3. methods (actions)
     openOverlay()
     {
         this.searchOverlay.addClass("search-overlay--active");
+        $("body").addClass("body-no-scroll");
+        this.isOverlayOpen = true;
     }
 
     closeOverlay()
     {
         this.searchOverlay.removeClass("search-overlay--active");
+        $("body").removeClass("body-no-scroll");
+        this.isOverlayOpen = false;
+    }
+
+    keyPressDispatcher(e)
+    {
+        if (e.keyCode == 83 && !this.isOverlayOpen) {
+            this.openOverlay();
+        }
+
+        if (e.keyCode == 27 && this.isOverlayOpen) {
+            this.closeOverlay();
+        }
     }
 }
 
