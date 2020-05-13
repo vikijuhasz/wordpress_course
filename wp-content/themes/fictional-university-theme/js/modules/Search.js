76acf2d565546f65a2e1d9cd6dd3a258203fd81b
Viktoria Juhasz <viktoriajuhasz8@gmail.com>
2020-05-13 10:34:22 +0200

    open and close search overlay

--- wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -1,8 +1,32 @@
+import $ from 'jquery';
+
 class Search
 {
+    // 1. describe/define properties and create/initiate our object
     constructor()
     {
-        alert('Hello, I am a search');
+        this.openButton = $(".js-search-trigger ");
+        this.closeButton = $(".search-overlay__close");
+        this.searchOverlay = $(".search-overlay");
+        this.events();
+    }
+
+    // 2. events
+    events()
+    {
+        this.openButton.on("click", this.openOverlay.bind(this));
+        this.closeButton.on("click", this.closeOverlay.bind(this));
+    }
+
+    // 3. methods (actions)
+    openOverlay()
+    {
+        this.searchOverlay.addClass("search-overlay--active");
+    }
+
+    closeOverlay()
+    {
+        this.searchOverlay.removeClass("search-overlay--active");
     }
 }
 
