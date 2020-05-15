8573fbb33e167f90ba39fd5cf3210a087d89d95c
Viktoria Juhasz <viktoriajuhasz8@gmail.com>
2020-05-15 13:24:19 +0200

    loading spinner icon

--- wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -5,12 +5,15 @@ class Search
     // 1. describe/define properties and create/initiate our object
     constructor()
     {
+        this.resultsDiv = $("#search-overlay__results");
         this.openButton = $(".js-search-trigger ");
         this.closeButton = $(".search-overlay__close");
         this.searchOverlay = $(".search-overlay");
         this.searchField = $("#search-term");
         this.events();
         this.isOverlayOpen = false;
+        this.isSpinnerVisible = false;
+        this.previousValue;
         this.typingTimer;
     }
 
@@ -20,16 +23,36 @@ class Search
         this.openButton.on("click", this.openOverlay.bind(this));
         this.closeButton.on("click", this.closeOverlay.bind(this));
         $(document).on("keydown", this.keyPressDispatcher.bind(this));
-        this.searchField.on("keydown", this.typingLogic.bind(this));
+        this.searchField.on("keyup", this.typingLogic.bind(this));
     }
 
     // 3. methods (actions)
     
     typingLogic()
     {
-        clearTimeout(this.typingTimer);
-        this.typingTimer = setTimeout(function() { console.log('This is a tiemout test') }, 2000);
+        if (this.searchField.val() != this.previousValue) {
+            clearTimeout(this.typingTimer);
+            if (this.searchField.val()) {
+                if (!this.isSpinnerVisible) {
+                    this.resultsDiv.html('<div class="spinner-loader"></div>');
+                    this.isSpinnerVisible = true;
+                }
+                this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
+            } else {
+                this.resultsDiv.html('');
+                this.isSpinnerVisible = false;
+            }
+        }
+        
+        this.previousValue = this.searchField.val();
     }
+    
+    getResults()
+    {
+        this.resultsDiv.html("Search results HERE");
+        this.isSpinnerVisible = false;
+    }
+    
     
     openOverlay()
     {
@@ -47,7 +70,7 @@ class Search
 
     keyPressDispatcher(e)
     {
-        if (e.keyCode == 83 && !this.isOverlayOpen) {
+        if (e.keyCode == 83 && !this.isOverlayOpen && !$("input, textarea").is(':focus')) {
             this.openOverlay();
         }
 
