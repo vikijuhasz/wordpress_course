99e5daa6ef9c8fe51cb488c4932eb2a841393300
Viktoria Juhasz <viktoriajuhasz8@gmail.com>
2020-05-16 09:36:30 +0200

    generate HTML based on JSON

--- wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -49,9 +49,14 @@ class Search
     
     getResults()
     {
-        $.getJSON('http://localhost/wordpress/wp-json/wp/v2/posts?search=' + this.searchField.val(), function(posts) {
-            alert(posts[0].title.rendered)
-        })
+        $.getJSON('http://localhost/wordpress/wp-json/wp/v2/posts?search=' + this.searchField.val(), posts => {
+            this.resultsDiv.html(`
+                <h2 class="search-overlay__section-title">General Information</h2>
+                <ul class="link-list min-list">
+                    ${posts.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join('')}
+                </ul>
+            `);
+        });
     }
     
     
