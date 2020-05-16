83c387bfb320962abeefce7c5d80be17f6bca252
Viktoria Juhasz <viktoriajuhasz8@gmail.com>
2020-05-16 12:07:27 +0200

    conditional logic within template literal

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -36,7 +36,10 @@ function university_files()
     wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
     wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-    wp_enqueue_style('university_main_styles', get_stylesheet_uri(), NULL, microtime());    
+    wp_enqueue_style('university_main_styles', get_stylesheet_uri(), NULL, microtime()); 
+    wp_localize_script('main-university-js', 'universityData', [
+        'root_url' => get_site_url()
+    ]);   
 }
 
 add_action('wp_enqueue_scripts', 'university_files');
