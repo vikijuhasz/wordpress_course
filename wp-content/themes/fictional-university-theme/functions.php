dc9539a51a3f783eb3457b7db8591d52df60a05d
Viktoria Juhasz <viktoriajuhasz8@gmail.com>
2020-05-17 11:30:47 +0200

    rest api: add new custom field

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -1,5 +1,15 @@
 <?php 
 
+function university_custom_rest() {
+    register_rest_field('post', 'authorName', [
+        'get_callback' => function () {
+            return get_the_author();
+        }
+    ]); 
+}
+
+add_action('rest_api_init', 'university_custom_rest');
+
 function pageBanner($args = NULL)
 {
     if (!$args['title']) {
