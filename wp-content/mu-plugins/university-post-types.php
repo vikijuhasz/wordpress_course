2f3a3418e75b29dcff91312f694ccc846d16dbc4
Viktoria Juhasz <viktoriajuhasz8@gmail.com>
2020-05-18 07:43:54 +0200

    rest api: add new custom route

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -52,6 +52,7 @@ function university_post_types()
 
     // Professor Post Type
     register_post_type('professor', [
+        'show_in_rest' => true,
         'supports' => ['title', 'editor', 'thumbnail'],
         'public' => true, 
         'labels' => [
---
+++ wp-content/themes/fictional-university-theme/includes/search-route.php
@@ -0,0 +1,16 @@
+<?php 
+
+add_action('rest_api_init', 'universityRegisterSearch');
+
+function universityRegisterSearch()
+{
+    register_rest_route('university/v1', 'search', [
+        'methods' => WP_REST_Server::READABLE,
+        'callback' => 'universitySearchResults'
+    ]);
+}
+
+function universitySearchResults()
+{
+    return 'Congratulations!';
+}
