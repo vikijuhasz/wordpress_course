5c670ed78809d3f54423b85da4e8673c2d33b11e
Viktoria Juhasz <viktoriajuhasz8@gmail.com>
2020-05-22 20:20:51 +0200

    complete search overlay

--- wp-content/themes/fictional-university-theme/includes/search-route.php
+++ wp-content/themes/fictional-university-theme/includes/search-route.php
@@ -45,6 +45,17 @@ function universitySearchResults($data)
         }
 
         if (get_post_type() == 'program') {
+            $relatedCampuses = get_field('related_campus');
+
+            if ($relatedCampuses) {
+                foreach($relatedCampuses as $campus) {
+                    array_push($results['campuses'], [
+                        'title' => get_the_title($campus), 
+                        'permalink' => get_the_permalink($campus)
+                    ]);
+                }
+            }
+
             array_push($results['programs'], [
                 'title' => get_the_title(),
                 'permalink' => get_the_permalink(),
@@ -89,21 +100,44 @@ function universitySearchResults($data)
             }        
             
             $programRelationshipQuery = new WP_Query([
-            'post_type' => 'professor',
+            'post_type' => ['professor', 'event'],
             'meta_query' => $programsMetaQuery
             ]);
             
             while($programRelationshipQuery->have_posts()) {
                 $programRelationshipQuery->the_post();
-                array_push($results['professors'], [
-                    'title' => get_the_title(),
-                    'permalink' => get_the_permalink(),
-                    'image' =>get_the_post_thumbnail_url(0, 'professorLandscape')
-                ]);
+
+                if (get_post_type() == 'event') {
+                    $eventDate = new DateTime(get_field('event_date'));
+                    $description = null;
+                    if (has_excerpt()) {
+                        $description = get_the_excerpt();
+                    } else {
+                        $description = wp_trim_words(get_the_content(), 18);
+                    } 
+        
+                    array_push($results['events'], [
+                        'title' => get_the_title(),
+                        'permalink' => get_the_permalink(), 
+                        'month' => $eventDate->format('M'),
+                        'day' => $eventDate->format('d'),
+                        'description' => $description
+                    ]);
+                }
+                
+                if (get_post_type() == 'professor') {
+                    array_push($results['professors'], [
+                        'title' => get_the_title(),
+                        'permalink' => get_the_permalink(),
+                        'image' =>get_the_post_thumbnail_url(0, 'professorLandscape')
+                    ]);
+                }
+                
             }
         }
 
         $results['professors'] = array_values(array_unique($results['professors'], SORT_REGULAR));
+        $results['events'] = array_values(array_unique($results['events'], SORT_REGULAR));
     }
         
     
