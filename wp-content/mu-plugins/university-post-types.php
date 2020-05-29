03cdfc364398478e1b3e7e10625096280ad15908
Viktoria Juhasz <viktoriajuhasz8@gmail.com>
2020-05-29 13:39:32 +0200

    my notes feature

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -68,6 +68,22 @@ function university_post_types()
         ], 
         'menu_icon' => 'dashicons-welcome-learn-more'
     ]);
+
+    // Note Post Type
+    register_post_type('note', [
+        'show_in_rest' => true,
+        'supports' => ['title', 'editor'],
+        'public' => false, 
+        'show_ui' => true,
+        'labels' => [
+            'name' => 'Notes',
+            'add_new_item' => 'Add New Note',
+            'edit_item' => 'Edit Note', 
+            'all_items' => 'All Notes', 
+            'singular_name' => 'Note'
+        ], 
+        'menu_icon' => 'dashicons-welcome-write-blog'
+    ]);
     
 }
 
