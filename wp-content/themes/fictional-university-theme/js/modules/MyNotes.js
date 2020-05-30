1ca4fb2e198bace691b20b444135a75a5af296e2
Viktoria Juhasz <viktoriajuhasz8@gmail.com>
2020-05-30 12:35:43 +0200

    delete posts

--- wp-content/themes/fictional-university-theme/js/modules/MyNotes.js
+++ wp-content/themes/fictional-university-theme/js/modules/MyNotes.js
@@ -10,18 +10,29 @@ class MyNotes
     events()
     {
         $(".delete-note").on("click", this.deleteNote);
+        $(".edit-note").on("click", this.editNote);
     }
 
     // Methods will go here 
-    deleteNote()
+
+    editNote(e)
     {
+        var thisNote = $(e.target).parent("li");
+        thisNote.find(".note-title-field, .note-body-field").removeAttr("readonly").addClass("note-active-field");
+        thisNote.find(".update-note").addClass("update-note--visible");
+    }
+
+    deleteNote(e)
+    {
+        var thisNote = $(e.target).parent("li");
         $.ajax({
             beforeSend: (xhr) => {
                 xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
             },
-            url: universityData.root_url + '/wp-json/wp/v2/note/152',
+            url: universityData.root_url + '/wp-json/wp/v2/note/' + thisNote.data('id'),
             type: 'DELETE',
             success: (response) => {
+                thisNote.slideUp();
                 console.log("Congrats");
                 console.log(response);
             },
