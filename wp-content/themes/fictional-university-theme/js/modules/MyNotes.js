1d5fd1c55a8f20b3f600bc0fce2feb8244a9a77e
Viktoria Juhasz <viktoriajuhasz8@gmail.com>
2020-05-31 17:45:06 +0200

    create new posts

--- wp-content/themes/fictional-university-theme/js/modules/MyNotes.js
+++ wp-content/themes/fictional-university-theme/js/modules/MyNotes.js
@@ -9,9 +9,10 @@ class MyNotes
 
     events()
     {
-        $(".delete-note").on("click", this.deleteNote);
-        $(".edit-note").on("click", this.editNote.bind(this));
-        $(".update-note").on("click", this.updateNote.bind(this));
+        $("#my-notes").on("click", ".delete-note", this.deleteNote);
+        $("#my-notes").on("click", ".edit-note", this.editNote.bind(this));
+        $("#my-notes").on("click", ".update-note", this.updateNote.bind(this));
+        $(".submit-note").on("click", this.createNote.bind(this));
     }
 
     // Methods will go here 
@@ -63,10 +64,46 @@ class MyNotes
         })
     }
 
+    createNote(e)
+    {
+        var ourNewPost = {
+            'title': $(".new-note-title").val(),
+            'content': $(".new-note-body").val(), 
+            'status': 'publish'
+        };
+
+        $.ajax({
+            beforeSend: (xhr) => {
+                xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
+            },
+            url: universityData.root_url + '/wp-json/wp/v2/note/',
+            type: 'POST',
+            data: ourNewPost,
+            success: (response) => {
+                $(".new-note-title, .new-note-body").val('');
+                $(`
+                    <li data-id="${response.id}">   
+                    <input readonly class="note-title-field" value="${response.title.raw}">
+                    <span class="edit-note"><i class="fa fa-pencil" aria-hidden="true"></i> Edit</span>
+                    <span class="delete-note"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</span>
+                    <textarea readonly class="note-body-field">${response.content.raw}</textarea>
+                    <span class="update-note btn btn--blue btn--small"><i class="fa fa-arrow-right" aria-hidden="true"></i> Save</span>
+                    </li>
+                `).prependTo("#my-notes").hide().slideDown();
+                console.log("Congrats");
+                console.log(response);
+            },
+            error: (response) => {
+                console.log("Sorry");
+                console.log(response);
+            } 
+        })
+    }
+
     updateNote(e)
     {
         var thisNote = $(e.target).parent("li");
-
+        
         var ourUpdatedPost = {
             'title': thisNote.find(".note-title-field").val(),
             'content': thisNote.find(".note-body-field").val()
--- wp-content/themes/fictional-university-theme/page-my-notes.php
+++ wp-content/themes/fictional-university-theme/page-my-notes.php
@@ -14,6 +14,13 @@ if (!is_user_logged_in()) {
         ?>
 
   <div class="container container--narrow page-section">
+
+    <div class="create-note">
+        <h2 class="headline headline--medium">Create New Note</h2>
+        <input class="new-note-title" placeholder="Title">
+        <textarea class="new-note-body" placeholder="Your note here..."></textarea>
+        <span class="submit-note">Create Note</span>
+    </div>    
     <ul class="min-list link-list" id="my-notes">
         <?php 
             $userNotes = new WP_Query([
