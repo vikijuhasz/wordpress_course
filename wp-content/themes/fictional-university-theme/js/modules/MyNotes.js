236293493120f9809c90b2494c5c115c08af66b0
Viktoria Juhasz <viktoriajuhasz8@gmail.com>
2020-05-31 16:05:12 +0200

    edit/update posts

--- wp-content/themes/fictional-university-theme/js/modules/MyNotes.js
+++ wp-content/themes/fictional-university-theme/js/modules/MyNotes.js
@@ -10,7 +10,8 @@ class MyNotes
     events()
     {
         $(".delete-note").on("click", this.deleteNote);
-        $(".edit-note").on("click", this.editNote);
+        $(".edit-note").on("click", this.editNote.bind(this));
+        $(".update-note").on("click", this.updateNote.bind(this));
     }
 
     // Methods will go here 
@@ -18,8 +19,27 @@ class MyNotes
     editNote(e)
     {
         var thisNote = $(e.target).parent("li");
+        if (thisNote.data("state") == "editable") {
+            this.makeNoteReadOnly(thisNote);
+        } else {
+            this.makeNoteEditable(thisNote);
+        }
+    }
+
+    makeNoteEditable(thisNote) 
+    {
+        thisNote.find(".edit-note").html('<i class="fa fa-times" aria-hidden="true"></i> Cancel');
         thisNote.find(".note-title-field, .note-body-field").removeAttr("readonly").addClass("note-active-field");
         thisNote.find(".update-note").addClass("update-note--visible");
+        thisNote.data("state", "editable");
+    }
+
+    makeNoteReadOnly(thisNote)
+    {
+        thisNote.find(".edit-note").html('<i class="fa fa-pencil" aria-hidden="true"></i> Edit');
+        thisNote.find(".note-title-field, .note-body-field").attr("readonly", "readonly").removeClass("note-active-field");
+        thisNote.find(".update-note").removeClass("update-note--visible");
+        thisNote.data("state", "cancel");
     }
 
     deleteNote(e)
@@ -33,6 +53,34 @@ class MyNotes
             type: 'DELETE',
             success: (response) => {
                 thisNote.slideUp();
+                console.log("Congrats");
+                console.log(response);
+            },
+            error: (response) => {
+                console.log("Sorry");
+                console.log(response);
+            } 
+        })
+    }
+
+    updateNote(e)
+    {
+        var thisNote = $(e.target).parent("li");
+
+        var ourUpdatedPost = {
+            'title': thisNote.find(".note-title-field").val(),
+            'content': thisNote.find(".note-body-field").val()
+        };
+
+        $.ajax({
+            beforeSend: (xhr) => {
+                xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
+            },
+            url: universityData.root_url + '/wp-json/wp/v2/note/' + thisNote.data('id'),
+            type: 'POST',
+            data: ourUpdatedPost,
+            success: (response) => {
+                this.makeNoteReadOnly(thisNote);
                 console.log("Congrats");
                 console.log(response);
             },
